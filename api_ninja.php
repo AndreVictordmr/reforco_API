4e1195ee7cd3de2b74ea2438cebc1134d3ffada3
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-10-30 15:07:51 -0300

    metodo POST

--- api_ninja.php
+++ api_ninja.php
@@ -1,6 +1,35 @@
 <?php
 
-    header('Context-Type: application/json: charset= UTF-8');
+    header('Context-Type: application/json charset= UTF-8');
     header('Access-Control-Allow-Origin:*');
 
     $metodo=$_SERVER['REQUEST_METHOD'];
+
+    switch($metodo){
+        case 'GET':
+                echo json_encode('metodo Get utilizado');
+            break;
+        case 'POST':
+                verificarCodigoSegreto();
+            break;
+        case 'PUT':
+        
+            break;
+        case 'DELETE':
+        
+            break;
+        default:
+
+            break;
+
+    }
+
+    function verificarCodigoSegreto(){
+        $chaveacesso=json_decode(file_get_contents("php://input"),true);
+
+        if($chaveacesso['codigo']==="ABCDE1234")
+            echo json_encode($chaveacesso['nome'].' a menssagem secreta é: Não existe menssagem secreta');
+        else
+            echo json_encode('Chave de acesso negada');
+        
+    }
--- cliente_ninja.php
+++ cliente_ninja.php
@@ -1,1 +1,21 @@
 <?php
+    $url = "http://localhost/reforco_api/api_ninja.php";
+
+    $chaveNinja = [
+        'codigo'=>'ABCDE1234',
+        'nome'=>"Gustavo"
+    ];
+
+    $estruturaHttp=[
+        'http'=>[
+            'method'=>"POST",
+            'header'=>"Content-Type: application/json\r\n",
+            'content'=>json_encode($chaveNinja)
+        ]
+    ];
+
+    $contexto = stream_context_create($estruturaHttp);
+
+    $resposta = file_get_contents($url,false,$contexto);
+
+    echo $resposta;
