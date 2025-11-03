ff907d5653031beb0fe3882c9acd7ab13ba69256
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-11-03 17:12:44 -0300

    Exercicio pesquisa

---
+++ exercicio/cliente_ai.php
@@ -0,0 +1,31 @@
+<?php
+    $api_key = 'AIzaSyAqKMAaUzEVBDJ53LK3svbcW20RkDdJW00';
+    $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=$api_key";
+
+    $content =[
+        'contents'=>[
+            'parts'=>[
+                'text'=>"Me explique o funcionamento do um motor"
+            ]
+        ]
+    ];
+    $estrutura=[
+        'http'=>[
+            'header'=>"Content-Type: application/json\r\n"."x-goog-api-key: $api_key\r\n",
+            'method'=>"POST",
+            'content'=>json_encode($content)
+        ]
+    ];
+
+    $enviar=stream_context_create($estrutura);
+
+    $resposta= file_get_contents($url,false,$enviar);
+    $resposta_json=json_decode($resposta,true);
+    
+    if(isset($resposta_json['candidates'][0]['content']['parts'][0]['text'])){
+        $texto= $resposta_json['candidates'][0]['content']['parts'][0]['text'];
+        echo '##Explicação do motor:\n<br>';
+        $text_html=nl2br($texto);
+    }else{
+        echo 'Erro: Não foi possível obter o texto da resposta. Verifique sua chave de API ou as permissões.';
+    }
